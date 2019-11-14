18b0f88ad6bd07defcca1fae2c1843d0755ea49c
Amaury Martiny <amaury.martiny@protonmail.com>
2019-11-14 21:56:44 +0100

    Improve coverage

--- jest.config.js
+++ jest.config.js
@@ -8,9 +8,10 @@ module.exports = {
   ...jestMongoDb,
   collectCoverageFrom: [
     '**/src/**/*.ts',
-    '!./packages/graphql/**',
-    '!**/index.ts',
-    '!**/testUtil.ts'
+    '!./packages/graphql/**', // These files are generated
+    '!**/index.ts', // index.ts only re-exports stuff
+    '!**/testUtil.ts', // These files are used in tests
+    '!./packages/microservices/**' // These files are hard to unit test
   ],
   setupFiles: ['./packages/microservices/test/util/setup.ts'],
   testEnvironment: 'node',
--- packages/dataproviders/src/normalized/normalized.spec.ts
+++ packages/dataproviders/src/normalized/normalized.spec.ts
@@ -1,29 +1,48 @@
+import { pipe } from 'fp-ts/lib/pipeable';
+import * as TE from 'fp-ts/lib/TaskEither';
+
+import { aqicnByGps, aqicnNormalizeByGps } from '../aqicn';
 import { generateRandomLatLng, testProvider } from '../util';
+import { waqiByGps, waqiNormalizeByGps } from '../waqi';
 import { normalizedByGps } from './fetchBy';
+import { LatLng, NormalizedByGps } from '../types';
+
+function additionalExpects(data: NormalizedByGps): void {
+  if (data.dailyCigarettes) {
+    expect(isNaN(data.dailyCigarettes)).toBe(false);
+  }
+
+  expect(data.closestStation.gps.latitude).toBeDefined();
+  expect(data.closestStation.gps.longitude).toBeDefined();
+  expect(data.closestStation.name).toBeDefined();
+  expect(data.closestStation.provider).toBe('waqi');
+  expect(data.closestStation.universalId.startsWith('waqi')).toBe(true);
+}
+
+function getOptions({ latitude, longitude }: LatLng) {
+  return {
+    additionalExpects,
+    fetchBy: 'gps' as const,
+    fetchById: `[${[latitude, longitude]}]`,
+    provider: 'normalized' as const
+  };
+}
 
 describe('normalized', () => {
   describe('by gps', () => {
-    [...Array(2)]
-      .map(generateRandomLatLng)
-      .forEach(({ latitude, longitude }) => {
-        testProvider(normalizedByGps({ latitude, longitude }), {
-          additionalExpects: data => {
-            if (data.dailyCigarettes) {
-              expect(isNaN(data.dailyCigarettes)).toBe(false);
-            }
+    [...Array(2)].map(generateRandomLatLng).forEach(gps => {
+      // Test normalization separately
+      testProvider(
+        pipe(aqicnByGps(gps), TE.map(aqicnNormalizeByGps)),
+        getOptions(gps)
+      );
+      testProvider(
+        pipe(waqiByGps(gps), TE.map(waqiNormalizeByGps)),
+        getOptions(gps)
+      );
 
-            expect(data.closestStation.gps.latitude).toBeDefined();
-            expect(data.closestStation.gps.longitude).toBeDefined();
-            expect(data.closestStation.name).toBeDefined();
-            expect(data.closestStation.provider).toBe('waqi');
-            expect(data.closestStation.universalId.startsWith('waqi')).toBe(
-              true
-            );
-          },
-          fetchBy: 'gps',
-          fetchById: `[${[latitude, longitude]}]`,
-          provider: 'normalized'
-        });
-      });
+      // Test normalization + race
+      testProvider(normalizedByGps(gps), getOptions(gps));
+    });
   });
 });
