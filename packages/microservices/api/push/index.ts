0c307f44051123dcfb82c5c15ebcaddbc1b8e875
Amaury Martiny <amaury.martiny@protonmail.com>
2019-11-17 17:38:00 +0100

    Remove some stuff

--- packages/microservices/api/push/index.ts
+++ packages/microservices/api/push/index.ts
@@ -1,9 +1,8 @@
 import { NowRequest, NowResponse } from '@now/node';
-import { aqicnByStation } from '@shootismoke/dataproviders';
-import { array } from 'fp-ts/lib/Array';
-import * as E from 'fp-ts/lib/Either';
-import * as T from 'fp-ts/lib/Task';
 
+// import { array } from 'fp-ts/lib/Array';
+// import * as E from 'fp-ts/lib/Either';
+// import * as T from 'fp-ts/lib/Task';
 import { HistoryItem } from '../../src/models';
 import { connectToDatabase } from '../../src/util';
 import { aggregation } from './aggregation';
@@ -17,21 +16,21 @@ export default async function push(
 
     const items = await HistoryItem.aggregate(aggregation);
 
-    const tasks = Array.from(
-      new Set(items.map(({ universalId }) => universalId))
-    ).map(aqicnByStation);
-    const responses = await array.sequence(T.task)(tasks)();
+    // const tasks = Array.from(
+    //   new Set(items.map(({ universalId }) => universalId))
+    // ).map(aqicnByStation);
+    // const responses = await array.sequence(T.task)(tasks)();
 
-    const correct = responses.filter(E.isRight).map(e =>
-      E.fold(
-        () => {
-          throw new Error('We already filtered isRight. qed.');
-        },
-        r => r
-      )(e)
-    );
+    // const correct = responses.filter(E.isRight).map(e =>
+    //   E.fold(
+    //     () => {
+    //       throw new Error('We already filtered isRight. qed.');
+    //     },
+    //     r => r
+    //   )(e)
+    // );
 
-    res.status(200).json([items, correct]);
+    res.status(200).json([items]);
   } catch (error) {
     res.status(400).send(error.message);
   }
