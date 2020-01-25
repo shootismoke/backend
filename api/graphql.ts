5c74e494c35b2e13965f91692ec3efcdfe27384f
Amaury Martiny <amaury.martiny@protonmail.com>
2020-01-25 15:43:47 +0100

    feat: Add sentry for bug tracking

--- api/graphql.ts
+++ api/graphql.ts
@@ -1,4 +1,12 @@
+import Sentry from '@sentry/node';
+
 import { nowApollo } from '../src/apollo';
 import { chain, hawk } from '../src/util';
+
+if (process.env.SENTRY_DSN) {
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN
+  });
+}
 
 export default chain(hawk)(nowApollo({ server: { path: '/api/graphql' } }));
