d66540ed2da4f73e8b3df83d4139f7bccd06e50f
Amaury Martiny <amaury.martiny@protonmail.com>
2020-02-13 17:30:35 +0100

    Fix lint

--- api/push.ts
+++ api/push.ts
@@ -25,7 +25,6 @@ export default async function(
       )
     ).flat();
 
-    // Self-explanatory
     const messages = await Promise.all(users.map(constructExpoMessage));
     const validMessages = messages.filter(isExpoPushMessage);
     const tickets = await sendBatchToExpo(validMessages);
