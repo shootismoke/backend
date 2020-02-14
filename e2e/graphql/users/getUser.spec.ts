d2a7ab6b996acf5e90173e373acb59b8bb5f6845
Amaury Martiny <amaury.martiny@protonmail.com>
2020-02-14 20:14:38 +0100

    feat: Add getUser graphql query (#142)

---
+++ e2e/graphql/users/getUser.spec.ts
@@ -0,0 +1,48 @@
+import { ALICE_ID, describeApollo, getAlice } from '../../util';
+import { GET_USER } from './gql';
+
+describeApollo('users::createUser', client => {
+  beforeAll(async done => {
+    await getAlice(client);
+
+    done();
+  });
+
+  it('should always require input', async done => {
+    const { query } = await client;
+
+    const res = await query({
+      query: GET_USER,
+      variables: {
+        expoInstallationId: 'foo'
+      }
+    });
+
+    expect(res.errors && res.errors[0].message).toContain(
+      'No user with expoInstallationId "foo" found'
+    );
+
+    done();
+  });
+
+  it('should always require input', async done => {
+    const { query } = await client;
+    const alice = await getAlice(client);
+
+    const res = await query({
+      query: GET_USER,
+      variables: {
+        expoInstallationId: ALICE_ID
+      }
+    });
+
+    if (!res.data) {
+      console.error(res);
+      return done.fail('No data in response');
+    }
+
+    expect(res.data.getUser).toEqual(alice);
+
+    done();
+  });
+});
--- src/typeDefs/user.ts
+++ src/typeDefs/user.ts
@@ -1,6 +1,10 @@
 import { gql } from 'apollo-server-micro';
 
 export const userTypeDefs = gql`
+  extend type Query {
+    getUser(expoInstallationId: ID!): User!
+  }
+
   extend type Mutation {
     createUser(input: CreateUserInput!): User!
     updateUser(expoInstallationId: ID!, input: UpdateUserInput!): User!
