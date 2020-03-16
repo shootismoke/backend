935805ae92de0c89a16b6d0b8c0d424cd8c82b16
Amaury Martiny <amaury.martiny@protonmail.com>
2020-03-16 22:37:53 +0100

    getUser return Maybe<User>

--- src/typeDefs/user.ts
+++ src/typeDefs/user.ts
@@ -2,12 +2,11 @@ import { gql } from 'apollo-server-micro';
 
 export const userTypeDefs = gql`
   extend type Query {
-    getUser(expoInstallationId: ID!): User!
+    getUser(expoInstallationId: ID!): User
   }
 
   extend type Mutation {
     createUser(input: CreateUserInput!): User!
-    getOrCreateUser(input: GetOrCreateUserInput!): User!
     updateUser(expoInstallationId: ID!, input: UpdateUserInput!): User!
   }
 `;
